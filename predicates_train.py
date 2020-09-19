845dddaf72bb77a88537c52494a884ccb4d1e143
zhaoyu <zhaoyu9067@live.cn>
2020-09-19 17:30:42 +0800

    vrd_test

--- predicates_train.py
+++ predicates_train.py
@@ -37,45 +37,14 @@ def train(**kwargs):
                                   # pin_memory=True,
                                   num_workers=opt.num_workers)
 
-    word2vec_map = load_from_word2vec("test_word2vec.txt")
-    word2vec_db = {"obj": [], "rel": []}
-    for obj in dataset.db.label_names:
-        if obj in word2vec_map:
-            word2vec_db["obj"].append(word2vec_map[obj])
-        else:
-            objs = obj.split(" ")
-            v = []
-            for i in objs:
-                v.append(word2vec_map[i])
-            v = numpy.array(v)
-            word2vec_db["obj"].append(numpy.average(v, axis=1))
-    for rel in dataset.db.predicates_name:
-        if rel in word2vec_map:
-            word2vec_db["rel"].append(word2vec_map[rel])
-        else:
-            rels = rel.split(" ")
-            v = []
-            for i in rels:
-                v.append(word2vec_map[i])
-            v = numpy.array(v)
-            word2vec_db["rel"].append(numpy.average(v, axis=1))
+    # word2vec_map = load_from_word2vec("test_word2vec.txt")
+    word2vec_db = json.load(open("w2v.json"))
 
     faster_rcnn = FasterRCNNVGG16()
     faster_rcnn_trainer = FasterRCNNTrainer(faster_rcnn)
     faster_rcnn_trainer.load(opt.faster_rcnn_model)
     vrd_trainer = VGG16PREDICATES(faster_rcnn_trainer, word2vec_db, dataset.db.triplets).cuda()
     optimizer = t.optim.Adam(vrd_trainer.parameters())
-
-
-    w2v = {
-        "obj": [],
-        "rel": []
-    }
-    for obj in dataset.db.label_names:
-        w2v["obj"].append(word2vec_db.get_vector(obj))
-
-    for rel in dataset.db.predicates_name:
-        w2v["rel"].append(word2vec_db.get_vector(rel))
 
     for epoch in range(opt.vrd_epoch):
         for ii, (img, D) in tqdm(enumerate(dataloader)):
